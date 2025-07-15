20631e7e867ba27e837002164cdc984aa1f6ee2e
vicente132 <vihernandez2022@udec.cl>
2025-07-15 08:57:51 -0400

    Ahora se puede agregar y eliminar cajas, implementado con su respectivo handling de excepciones

--- Main.py
+++ Main.py
@@ -1,36 +1,6 @@
 from tkinter import *
 import sqlite3
-
-
-def agregar_ubicaciones():
-    nombre = entry_nombre_ubi.get()
-    conn = sqlite3.connect("organize.db")
-    cursor = conn.cursor()
-
-    cursor.execute("Insert into ubicaciones (nombre) values (?)", (nombre,))
-    conn.commit()
-    conn.close()
-    print("contenedor agregado")
-
-
-def eliminar_ubicacion():
-    nombre = entry_nombre_ubi_del.get()
-    conn = sqlite3.connect("organize.db")
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT nombre FROM ubicaciones WHERE nombre = (?)", (nombre,))
-    if not cursor.fetchone():
-        print(f"Error: No existe una ubicacion con Nombre: {nombre}")
-        return False
-
-    cursor.execute("Delete from ubicaciones Where nombre = (?)", (nombre,))
-
-    conn.commit()
-    print(f"Caja con ID {nombre} eliminada correctamente")
-
-    conn.commit()
-    conn.close()
-    print("ubicacion eliminada")
+from creaciones import *
 
 
 def mostrar_contenedores():
@@ -53,8 +23,8 @@ Label(root, text="Quitar ubicacion").pack()
 entry_nombre_ubi_del = Entry(root)
 entry_nombre_ubi_del.pack()
 
-Button(root, text="agregar ubicacion", command=agregar_ubicaciones).pack()
+Button(root, text="agregar ubicacion", command=agregar_ubicaciones(entry_nombre_ubi.get())).pack()
 Button(root, text="Ver ubiaciones", command=mostrar_contenedores).pack()
+Button(root, text="Eliminar ubicación", command=eliminar_ubicacion(entry_nombre_ubi_del.get())).pack()
 
-Button(root, text="Eliminar ubicación", command=eliminar_ubicacion).pack()
 root.mainloop()
---
+++ creaciones.py
@@ -0,0 +1,56 @@
+import __main__
+import sqlite3
+
+
+def agregar_ubicaciones(nombre):
+    conn = sqlite3.connect("organize.db")
+    cursor = conn.cursor()
+
+    cursor.execute("Insert into ubicaciones (nombre) values (?)", (nombre,))
+    conn.commit()
+    conn.close()
+    print(f"contenedor {nombre} agregado.")
+
+
+def eliminar_ubicacion(nombre_elim):
+    conn = sqlite3.connect("organize.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT nombre FROM ubicaciones WHERE nombre = (?)", (nombre_elim,))
+    if not cursor.fetchone():
+        print(f"Error: No existe una ubicacion con nombre: {nombre_elim}")
+        return False
+
+    cursor.execute("Delete from ubicaciones Where nombre = (?)", (nombre_elim,))
+
+    conn.commit()
+    print(f"Caja con ID {nombre_elim} eliminada correctamente")
+
+    conn.commit()
+    conn.close()
+    print("ubicacion eliminada")
+
+
+def agregar_caja(nombre):
+    conn = sqlite3.connect("organize.db")
+    cursor = conn.cursor()
+
+    cursor.execute("Insert into cajas (nombre) values (?)", (nombre, ))
+    conn.commit()
+    conn.close()
+    print(f"contenedor {nombre} agregado.")
+
+
+def aliminar_caja(nombre_elim):
+    conn = sqlite3.connect("organize.db")
+    cursor = conn.cursor()
+
+    cursor.execute("Selecto from cajas where nombre = (?)", (nombre_elim, ))
+    if not cursor.fetchone():
+        print(f"Error: No existe una caja con el nombre: {nombre_elim}")
+        return False
+
+    conn.commit()
+    conn.close()
+    print("Caja eliminada")
+    
